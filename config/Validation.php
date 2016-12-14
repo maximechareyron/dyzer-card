5bd18ff30821ae6f05d2bef42900273f65dec47e
Krakinette <wgokraken@gmail.com>
2016-12-14 16:47:44 +0100

    Add files via upload

--- config/Validation.php
+++ config/Validation.php
@@ -4,26 +4,31 @@ namespace config;
 
 class Validation {
 
-static function val_action($action) {
+    static function val_action($action) {
 
-if (!isset($action)) { throw new Exception('pas d\'action');}
+        if (!isset($action)) { throw new Exception('pas d\'action');}
 
-}
+    }
 
-static function val_form($nom,$age,&$dVueEreur) {
+    static function val_form($nom,$age,&$dVueEreur) {
 
-if (!isset($nom)||$nom=="") {
-$dVueEreur[] =	"pas de nom";
-$nom="";
-}
+        if (!isset($nom)||$nom=="") {
+            $dVueEreur[] =	"pas de nom";
+            $nom="";
+        }
 
-if (!isset($age)||$age==""||!filter_var($age, FILTER_VALIDATE_INT)) {
-$dVueEreur[] =	"pas d'age ";
-$age=0;
-}
+        if (!isset($age)||$age==""||!filter_var($age, FILTER_VALIDATE_INT)) {
+            $dVueEreur[] =	"pas d'age ";
+            $age=0;
+        }
 
-}
-
+    }
+    static function nettoyer_string($string)
+    {
+        $string=filter_var($string, FILTER_SANITIZE_STRING);
+        return $string;
+    }
+    
 }
 ?>
 
