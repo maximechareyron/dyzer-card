c0c7ad454d11e00a949a81a8a039061cb5d45aea
Maxime CHAREYRON <maxime.chareyron@etu.udamail.fr>
2016-12-14 17:01:56 +0100

    modifs

--- controleur/CtrlUser.php
+++ controleur/CtrlUser.php
@@ -6,12 +6,6 @@ class CtrlUser {
 
 	function __construct() {
 
-		// Démarrage (ou reprise) de la session
-		session_start();
-
-
-// Début
-
 		// On initialise un tableau d'erreur :
 		$dVueEreur = array ();
 
@@ -22,39 +16,42 @@ class CtrlUser {
 
 				// Pas d'action, on réinitialise 1er appel
 				case NULL:
-				$this->Reinit();
-				break;
+					$this->Reinit();
+					break;
+
+				case 'ajoutCommentaire':
+					$this->ajouterCommentaire();
+					break;
+
 
 				// Mauvaise action
 				default:
-				$dVueEreur[] =	"Erreur d'appel php";
-				require ($rep.$vues['erreur']);
-				break;
+					$dVueEreur[] =	"Erreur d'appel php";
+					require ($rep.$vues['erreur']);
+					break;
 			}
-
-		} catch (PDOException $e)
-		{
-	//si erreur BD, pas le cas ici
-			$dVueEreur[] =	"Erreur inattendue!!! ";
-			require ($rep.$vues['erreur']);
-
 		}
-		catch (Exception $e2)
-		{
-			$dVueEreur[] =	"Erreur inattendue!!! ";
+		catch (PDOException $e){
+			//si erreur BD, pas le cas ici
+			$dVueEreur[] =	"Erreur inattendue ! ";
 			require ($rep.$vues['erreur']);
 		}
-
-
-	//fin
+		catch (Exception $e2){
+			$dVueEreur[] =	"Erreur inattendue ! ";
+			require ($rep.$vues['erreur']);
+		}
 		exit(0);
-	}//fin constructeur
+	}
 
 
-	function Reinit() {
+	function Reinit(){
 		global $rep,$vues;
 
 		require ($rep.$vues['accueil']);
+	}
+
+	function AjouterCommentaire(){
+		
 	}
 }
 
--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 require_once(__DIR__.'/config/config.php');
 
 //Chargement de l'autoloader (le plus simple)
-require_once(__DIR__.'/config/Autoload.php');
-Autoload::charger();
+// require_once(__DIR__.'/config/Autoload.php');
+// Autoload::charger();
 
 
 // autoloader conforme norme PSR-0
-// require_once(__DIR__.'/config/SplClassLoader.php');
-// $myLibLoader = new SplClassLoader('controleur', './');
-// $myLibLoader->register();
-// $myLibLoader = new SplClassLoader('config', './');
-// $myLibLoader->register();
-// $myLibLoader = new SplClassLoader('modeles', './');
-// $myLibLoader->register();
+require_once(__DIR__.'/config/SplClassLoader.php');
+$myLibLoader = new SplClassLoader('controleur', './');
+$myLibLoader->register();
+$myLibLoader = new SplClassLoader('config', './');
+$myLibLoader->register();
+$myLibLoader = new SplClassLoader('modeles', './');
+$myLibLoader->register();
 
 
 // Démarrage (ou reprise) de la session
