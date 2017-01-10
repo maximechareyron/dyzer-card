3b325f0ee8959effaac71dfb12495152d9aad745
Krakinette <wgokraken@gmail.com>
2017-01-10 17:54:55 +0100

    Add files via upload

--- Metier/Commentaire.php
+++ Metier/Commentaire.php
@@ -5,3 +5,25 @@
  * Date: 04/01/17
  * Time: 16:06
  */
+namespace DyzerCard\Metier;
+
+use DyzerCard\Config\Config;
+
+class Commentaire
+{
+    public $idMusique;
+    public $idUser;
+    public $date;
+    public $text;
+
+    public function __construct($musicID, $userID, $date,$text)
+    {
+        $this->idMusique = $musicID;
+        $this->idUser = $userID;
+        $this->date = $date;
+        $this->text = $text;
+    }
+
+
+
+}
